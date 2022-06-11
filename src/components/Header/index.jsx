585a9fa97610f9716b5d1271c80b5006f438b8ff
Jean Flores <jeansfloresof@gmail.com>
2022-06-10 23:28:58 -0300

    initial setup

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as S from './Header.module.css';
+
+export function Header() {
+  return (
+    <header className={S.header}>
+      <strong>Ignite Feed</strong>;
+    </header>
+  )
+}
